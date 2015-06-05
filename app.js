32a48a4ce14cce9d1605fdf771a681dab9924e77
joanpaon <joanpaon@gmail.com>
2015-06-05 13:08:31 +0200

    Primera página y favicon

--- app.js
+++ app.js
@@ -1,35 +1,99 @@
+// Importa los módulos necesarios para la aplicación
+// ---
+// Express es un framework web mínimo y flexible para Node.js
+// que proporciona un conjunto robusto de características para
+// aplicaciones web y móviles.
 var express = require('express');
+
+// This module contains utilities for handling and transforming
+// file paths. Almost all these methods perform only string
+// transformations.
+// The file system is not consulted to check whether paths are valid.
 var path = require('path');
+
+// El módulo "serve-favicon" es un MW para Node.js que permite
+// servir un favicon.
+// ---
+// Un favicon es un indicador visual que el software cliente, como
+// navegadores, usan para identificar un sitio.
+// ---
+// Los agentes de usuario hacen peticiones de favicon.ico
+// frecuentemente e indiscriminadamente, por lo cual es deseable
+// excluir estas peticiones de los registros usando este MW
+// previamente al MW de registro (log).
+// ---
+// Este módulo cachea el icono en memoria para mejorar el rendimiento
+// saltándose el acceso a disco
+// --
+// This module provides an ETag based on the contents of the icon,
+// rather than file system properties.
+// ---
+// Este módulo servirá con el indicador "Content-Type" más compatible.
 var favicon = require('serve-favicon');
+
+// HTTP request logger middleware for node.js
 var logger = require('morgan');
+
+// Parse Cookie header and populate req.cookies with an object
+// keyed by the cookie names.
+// Optionally you may enable signed cookie support by passing a secret
+// string, which assigns req.secret so it may be used by other middleware.
 var cookieParser = require('cookie-parser');
+
+// Node.js body parsing middleware.
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Importa los enrutadores como si fueran módulos
+// En la referencia a estos módulos
+//  > La extensión ".js" se puede omitir
+//  > Hay que indicar la ruta a partir de la carpeta donde está
+//    este archivo (app.js), explicitamente "./"
+var routes = require('./routes/index'); // index.js
+//var users = require('./routes/users');  // users.js
 
+// Genera la aplicación
 var app = express();
 
-// view engine setup
+// Before Express can render template files,
+// the following application settings have to be set.
+// ---
+// Ruta de localización de las vistas
 app.set('views', path.join(__dirname, 'views'));
+
+// Motor para las vistas - Embedded Javascript
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+
+// Monta MWs - Por omisión de 'path' para cualquier ruta
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
+
+// Monta MWs - static - Por omisión de 'path' para cualquier ruta
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Monta MWs enrutadores
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Cualquier otra ruta no cazada por los enrutadores
+app.use(function (req, res, next) {
+  // Instancia un nuevo error
+  var err = new Error('Not Found');
+
+  // Parametriza el error
+  err.status = 404;
+
+  // Delega el error
+  next(err);
 });
 
 // error handlers
@@ -37,24 +101,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-
+// Exporta la aplicación
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
+// Carga los modulos necesarios para la aplicación
 var express = require('express');
+
+// Un objeto "router" es una instancia aislada de middleware y rutas.
+// Puede considerarse como una “mini-aplicación,” capacitada únicamente
+// para funciones de middleware y enrutado.
+// Cada aplicación Express tiene un enrutador incorporado.
+// ---
+// Un enrutador se comporta en sí mismo como middleware, por lo cual
+// puede usarse como un argumento de app.use() o como argumento en el
+// método use() de otro enrutador
+// ---
+// El objeto "express" de alto nivel tiene una función Router() que
+// crea un nuevo objeto "router".
+// ---
+// Una aplicación Express es esencialmente una serie de llamadas middleware.
+// ---
+// Un middleware es una función con acceso a:
+//  > Objeto "request" (req)
+//  > Objeto "response" (res)
+//  > El siguiente middleware de la secuencia en el ciclo "request-response"
+//    de una aplicación Express, comunmente denotado por una variable
+//    llamada "next"
+// ---
+// El middleware puede:
+//  > Ejecutar cualquier código
+//  > Hacer cambios en los objetos request/response.
+//  > Terminar el ciclo request-response.
+//  > Llamar al siguiente middleware de la pila.
+// ---
+// Si el MW actual no termina el ciclo request-response, debe de llamar a la
+// función next() para pasar el control al siguiente MW, de otro modo la
+// petición se quedará colgada sin respuesta.
+// ---
+// Con una ruta opciónal de montaje, el MW puede ser cargado en el nivel de
+// La aplicación o a nivel de enrutado. Asímismo, se puede cargar
+// conjuntamenteuna serie de funciones MW creado una subsecuencia de MW de
+// sistema en el punto de montaje.
+// ---
+// Una aplicación Express puede utilizar los siguientes tipos de MW:
+//  > De nivel de aplicación
+//  > De nivel de enrutado
+//  > De gestión de errores
+//  > MW incorporado
+//  > De terceras partes
+// ---
+// El MW de nivel de aplicación se enlaza a una instancia de express
+// utilizando el método app.use()
+// ---
+// El MW de nivel de enrutado trabaja igual que su homónimo de nivel de
+// aplicación excepto por el hecho de estar ligado a una instancia de
+// express.Router().
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+// Patrón en la ruta de la petición
+var patronEnrutado = '/';
 
+// Nombre de la vista
+var nombreVista = 'index';
+
+// Parámetros de la vista
+var paramVista = {
+  title: 'Quiz'
+};
+
+// Listener MW
+function renderizarVista(req, res) {
+  // Renderiza la vista 'index' usando objIds
+  res.render(nombreVista, paramVista);
+}
+
+// MW de enrutado
+router.get(patronEnrutado, renderizarVista);
+
+// Exporta el enrutador
 module.exports = router;
